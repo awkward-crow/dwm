9d47cfe25ca67d407a989876c25f7c5f57ec34c5
awkward crow <awkward.crow@gmail.com>
2023-04-27 15:48:37 +0100

    add appearance.light.h

---
+++ appearance.light.h
@@ -0,0 +1,20 @@
+/* See LICENSE file for copyright and license details. */
+
+// appearance.light based on solarised light colour scheme
+
+
+/* appearance */
+static const char font[]            = "-*-*-medium-*-*-*-14-*-*-*-*-*-*-*";
+static const char normbordercolor[] = "#eee8d5"; // S_base2
+static const char normbgcolor[]     = "#eee8d5"; // S_base2
+static const char normfgcolor[]     = "#657b83"; // S_base00
+static const char selbordercolor[]  = "#268bd2"; // S_blue
+static const char selbgcolor[]      = "#eee8d5"; // S_base2
+static const char selfgcolor[]      = "#073642"; // S_base02
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const Bool showbar           = True;     /* False means no bar */
+static const Bool topbar            = True;     /* False means bottom bar */
+
+
+// end
